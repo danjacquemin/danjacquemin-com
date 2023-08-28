fe765234d8c05282ec7c5ce2627850bd87f9d266
Dan Jacquemin <jacquemin@gmail.com>
2023-08-27 21:51:25 -0400

    added link to tailwind 411

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import { Seo } from '../components/seo';
-// import { Link } from 'gatsby';
+import { Link } from 'gatsby';
+
+import * as styles from './index.module.scss';
+
+import logoTailwindSvg from '../images/tailwindcss-logotype.svg';
 
 // -- -- --
 
@@ -19,7 +23,23 @@ const IndexPage = () => {
         <div className="wrapContents">
           <p>What the heck is going on here?!</p>
           <p>I'm rebuilding stuff and leaving cool notes. It is a process.</p>
+          <hr />
+          <div>
+            <p>
+              While I'm building things, I like to take semi-formal notes on the
+              technologies I use. This process has helped me immensely, and I
+              hope my notes can help you too.
+            </p>
+            <ul className={styles.techList}>
+              <li>
+                <a href="/tailwind/">
+                  <img src={logoTailwindSvg} alt="" height="25" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
+
         <aside className="wrapAside">♬♬ muzak ♬♬</aside>
       </div>
     </Layout>
